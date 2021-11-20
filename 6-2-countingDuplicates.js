60d9bed2ae7eb20401d12a9190ccb3bc12f91904
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-20 17:13:30 +0200

    update variables

--- 6-2-countingDuplicates.js
+++ 6-2-countingDuplicates.js
@@ -1,6 +1,6 @@
 const countDuplicate = (str) => {
-    let arr = str.toLowerCase().split('');
-    let duplicates =  arr.filter((value, index) => arr.indexOf(value) !== index);
+    let txt = str.toLowerCase().split('');
+    let duplicates =  txt.filter((value, index) => txt.indexOf(value) !== index);
     let result = duplicates.filter((value, index) => duplicates.indexOf(value) === index);
     return result.length; 
 }
